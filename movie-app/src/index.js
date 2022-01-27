3c9b01d8141b6159e0a23245e6c2c713e26b027b
kunallverma007 <58910303+kunallverma007@users.noreply.github.com>
2022-01-28 02:37:39 +0530

    Update index.js

--- movie-app/src/index.js
+++ movie-app/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+if (process.env.NODE_ENV !== "development")
+    console.log = () => {};
 ReactDOM.render(
   <React.StrictMode>
     <App />
